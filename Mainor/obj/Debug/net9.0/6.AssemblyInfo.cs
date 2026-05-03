63cd6628e37a6049ecf8fac14a1580a0fd088168
stepna41k <russia.zont@gmail.com>
2026-05-03 21:30:08 +0300

    chore: remove unused files

--- Mainor/obj/Debug/net9.0/6.AssemblyInfo.cs
+++ Mainor/obj/Debug/net9.0/6.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0104a2e97e6f0e8a9f68b8a53ca2b1ea5e7ba3ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cb17bab0e6a0266838a68654522f3cc33056db3")]
 [assembly: System.Reflection.AssemblyProductAttribute("6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
